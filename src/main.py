5b02e700c3e684e5ac9479c90195cbb2b551376a
RouhaLee <sumofi@naver.com>
2024-09-03 15:51:33 +0900

    Add project settings and integrate with main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
+from config.settings import DEBUG, VERSION
 from utils.helpers import greet
 
 def main():
+    print(f'Debug: {DEBUG}, Version: {VERSION}')
     print(greet('World'))
 
 if __name__ == '__main__':
